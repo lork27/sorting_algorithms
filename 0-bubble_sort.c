d7073bbea1f19088cf7627722661915d8639111e
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-25 17:28:43 -0400

    added swap function and changed logic so it won't check an index that doesn´t exist

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,15 +1,26 @@
 #include "sort.h"
 
 /**
+ *swap - function that sorts array
+ *@array: array that will be sorted
+ *@size: size of the array
+ */
+void swap(int *arr, int idx, int idy)
+{
+  int tmp;
+
+  tmp = arr[idx];
+  arr[idx] = arr[idy];
+  arr[idy] = tmp;
+}
+/**
  *bubble_sort - function that sorts array
  *@array: array that will be sorted
  *@size: size of the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
 	unsigned long int i, j;
-	int tmp;
 	int flag = 0;
 
   if (array == NULL || size < 2)
@@ -21,11 +32,9 @@ void bubble_sort(int *array, size_t size)
 		for (j = 0; j < size - 1; j++)
 		{
 
-			if (array[j] > array[j + 1])
+			if (array[j - 1] > array[j])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+        swap(array, j - 1, j);
 				flag = 1;
 				print_array(array, size);
 			}
