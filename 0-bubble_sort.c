7d4cd20428a1e6fb945d80785826de8e847df7f2
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-25 21:36:23 +0000

    fixed betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,16 +2,17 @@
 
 /**
  *swap - function that sorts array
- *@array: array that will be sorted
- *@size: size of the array
+ *@arr: array that will be sorted
+ *@idx: first element
+ *@idy: second elemtn
  */
 void swap(int *arr, int idx, int idy)
 {
-  int tmp;
+	int tmp;
 
-  tmp = arr[idx];
-  arr[idx] = arr[idy];
-  arr[idy] = tmp;
+	tmp = arr[idx];
+	arr[idx] = arr[idy];
+	arr[idy] = tmp;
 }
 /**
  *bubble_sort - function that sorts array
@@ -23,8 +24,8 @@ void bubble_sort(int *array, size_t size)
 	unsigned long int i, j;
 	int flag = 0;
 
-  if (array == NULL || size < 2)
-    return;
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -34,7 +35,7 @@ void bubble_sort(int *array, size_t size)
 
 			if (array[j - 1] > array[j])
 			{
-        swap(array, j - 1, j);
+				swap(array, j - 1, j);
 				flag = 1;
 				print_array(array, size);
 			}
