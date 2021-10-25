a1d50835aa3d8b530bd973c2ca75d8f4b9a71979
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-25 16:05:53 +0000

    first commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - function that sorts array
+ *@array: array that will be sorted
+ *@size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int i, j;
+	int tmp;
+	int flag = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			print_array(array, size);
+
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				flag = 1;
+			}
+			if (flag == 0)
+				continue;
+		}
+	}
+}
