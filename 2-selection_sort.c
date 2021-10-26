7df2f4377f0b1e0808b995a42654ee2f10e444a7
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-26 03:50:38 +0000

    selection sort implementation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,21 +15,18 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		if (i > 0)
-			print_array(array, size);
 		position = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[position] > array[j])
 				position = j;
-
 		}
 		if (position != i)
 		{
 			swap = array[i];
 			array[i] = array[position];
+			print_array(array, size);
 			array[position] = swap;
-
 		}
 
 	}
