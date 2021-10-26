8c33797bf49791003073a466a68bf7413744e241
lork27 <g.lorca.lamadrid@gmail.com>
2021-10-26 03:36:17 +0000

    selection sort implementation

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ *selection_sort - sort array in ascending order
+ *@array: to be sorted
+ *@size: of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned long int i, j, position, swap;
+
+	for (i = 0; i < size - 1; i++)
+	{
+
+		position = i;
+
+		print_array(array, size);
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[position] > array[j])
+				position = j;
+
+		}
+		if (position != i)
+		{
+			swap = array[i];
+			array[i] = array[position];
+			array[position] = swap;
+
+		}
+
+	}
+}
